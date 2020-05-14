8aa59220650bb39755e0af842ab61894678fb6ed
maxisuppes <msuppes@eryx.co>
2020-05-14 00:24:33 -0300

    Login de un usuario usando API pública:
    - Nueva pantalla de login con diseño básico
    - Se loguea a un usuario y se guarda su token en el localStorage

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Home} from "../controllers/Home";
 import {Profile} from "../controllers/Profile";
+import {Login} from "../controllers/Login";
 
 class Routes extends Component {
     render() {
         return (
             <Router key="router">
+                <Route exact path={"/"} component={Login}/>
                 <Route exact path={"/home"} component={Home}/>
                 <Route exact path={"/profile"} component={Profile}/>
             </Router>
